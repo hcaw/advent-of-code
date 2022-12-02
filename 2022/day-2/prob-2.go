611dbd24f11a09b597444d8d04ea5f4374da6ad7
Harry Wright <harry.wright@and.digital>
2022-12-02 11:29:49 +0000

    Add prob 2 for day 2

---
+++ 2022/day-2/prob-2.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// A = Rock, B = Paper, C = Scissors
+// X = Lose, Y = Draw, Z = Win
+
+// score for the shape you selected:
+// 1 for Rock, 2 for Paper, 3 for Scissors
+// score for the outcome of the round:
+// 0 for lose, 3 for draw, 6 for win
+
+type Round struct {
+	me  string
+	you string
+}
+
+func getRounds(lines []string) []Round {
+	var rounds []Round
+	for _, line := range lines {
+		first := string(line[0])
+		second := string(line[2])
+		round := Round{second, first}
+		rounds = append(rounds, round)
+	}
+	return rounds
+}
+
+func scoreRound(round Round) int {
+	score := 0
+	switch round.me {
+	// Lose
+	case "X":
+		switch round.you {
+		case "A":
+			score += 3
+		case "B":
+			score += 1
+		case "C":
+			score += 2
+		}
+	// Draw
+	case "Y":
+		score += 3
+		switch round.you {
+		case "A":
+			score += 1
+		case "B":
+			score += 2
+		case "C":
+			score += 3
+		}
+		// Win
+	case "Z":
+		score += 6
+		switch round.you {
+		case "A":
+			score += 2
+		case "B":
+			score += 3
+		case "C":
+			score += 1
+		}
+	}
+	return score
+}
+
+func main() {
+	input, _ := os.ReadFile("./input.txt")
+	lines := strings.Split(string(input), "\n")
+	rounds := getRounds(lines)
+
+	totalScore := 0
+	for _, round := range rounds {
+		totalScore += scoreRound(round)
+	}
+	fmt.Println(totalScore)
+}
