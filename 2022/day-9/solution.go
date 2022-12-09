149b838f412938833bf38074bf9e38dd9e49bc35
Harry Wright <harry.wright@and.digital>
2022-12-09 13:02:49 +0000

    Start of prob 2

--- 2022/day-9/solution.go
+++ 2022/day-9/solution.go
@@ -19,10 +19,7 @@ func abs(n int) int {
 	return n
 }
 
-func main() {
-	input, _ := os.ReadFile("./input.txt")
-	lines := strings.Split(string(input), "\n")
-	transforms := map[string]Coords{"L": {-1, 0}, "R": {1, 0}, "U": {0, 1}, "D": {0, -1}}
+func prob1(lines []string) int  {
 	h, t := Coords{}, Coords{}
 	visited := make(map[Coords]struct{})
 	visited[t] = struct{}{}
@@ -53,5 +50,50 @@ func main() {
 			visited[t] = struct{}{}
 		}
 	}
-	fmt.Println("Solution to problem 1 is", len(visited))
+	return len(visited)
+}
+
+func calcVisitedPositions(lines []string, ropeLen int) int  {
+	h, t := Coords{}, Coords{}
+	visited := make(map[Coords]struct{})
+	visited[t] = struct{}{}
+	for _, line := range lines {
+		direction := strings.Fields(line)[0]
+		steps, _ := strconv.Atoi(strings.Fields(line)[1])
+		for i := 0; i < steps; i++ {
+			h.x = h.x + transforms[direction].x
+			h.y = h.y + transforms[direction].y
+			diff := Coords{h.x - t.x, h.y - t.y}
+			if abs(diff.x) == 2 || abs(diff.y) == 2 {
+				if diff.x == 0 || diff.y == 0 {
+					t.x = t.x + transforms[direction].x
+					t.y = t.y + transforms[direction].y
+				} else {
+					if diff.x > 0 {
+						t.x = t.x + 1
+					} else {
+						t.x = t.x - 1
+					}
+					if diff.y > 0 {
+						t.y = t.y + 1
+					} else {
+						t.y = t.y - 1
+					}
+				}
+			}
+			visited[t] = struct{}{}
+		}
+	}
+	return len(visited)
+}
+
+var transforms = map[string]Coords{"L": {-1, 0}, "R": {1, 0}, "U": {0, 1}, "D": {0, -1}}
+
+func main() {
+	input, _ := os.ReadFile("./input.txt")
+	lines := strings.Split(string(input), "\n")
+	prob1 := prob1(lines)
+	fmt.Println("Solution to problem 1 is", prob1)
+	// prob2 := calcVisitedPositions(lines, 10)
+	// fmt.Println("Solution to problem 2 is", prob2)
 }
