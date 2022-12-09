fb77d52cd99d3b74eb8508a43d38feeeddc24c5f
Harry Wright <harry.wright@and.digital>
2022-12-09 15:06:47 +0000

    Solution to problem 2

--- 2022/day-9/solution.go
+++ 2022/day-9/solution.go
@@ -19,69 +19,40 @@ func abs(n int) int {
 	return n
 }
 
-func prob1(lines []string) int  {
-	h, t := Coords{}, Coords{}
+func calcVisitedPositions(lines []string, length int) int {
+	knots := make([]Coords, length)
 	visited := make(map[Coords]struct{})
-	visited[t] = struct{}{}
+	visited[Coords{}] = struct{}{}
 	for _, line := range lines {
 		direction := strings.Fields(line)[0]
 		steps, _ := strconv.Atoi(strings.Fields(line)[1])
 		for i := 0; i < steps; i++ {
-			h.x = h.x + transforms[direction].x
-			h.y = h.y + transforms[direction].y
-			diff := Coords{h.x - t.x, h.y - t.y}
-			if abs(diff.x) == 2 || abs(diff.y) == 2 {
-				if diff.x == 0 || diff.y == 0 {
-					t.x = t.x + transforms[direction].x
-					t.y = t.y + transforms[direction].y
+			knots[0].x = knots[0].x + transforms[direction].x
+			knots[0].y = knots[0].y + transforms[direction].y
+			for j := 1; j < length; j++ {
+				h, t := &knots[j-1], &knots[j]
+				diff := Coords{h.x - t.x, h.y - t.y}
+				if abs(diff.x) == 2 || abs(diff.y) == 2 {
+					if diff.x == 0 || diff.y == 0 {
+						t.x = t.x + (diff.x / 2)
+						t.y = t.y + (diff.y / 2)
+					} else {
+						if diff.x > 0 {
+							t.x = t.x + 1
+						} else {
+							t.x = t.x - 1
+						}
+						if diff.y > 0 {
+							t.y = t.y + 1
+						} else {
+							t.y = t.y - 1
+						}
+					}
 				} else {
-					if diff.x > 0 {
-						t.x = t.x + 1
-					} else {
-						t.x = t.x - 1
-					}
-					if diff.y > 0 {
-						t.y = t.y + 1
-					} else {
-						t.y = t.y - 1
-					}
+					break
 				}
 			}
-			visited[t] = struct{}{}
-		}
-	}
-	return len(visited)
-}
-
-func calcVisitedPositions(lines []string, ropeLen int) int  {
-	h, t := Coords{}, Coords{}
-	visited := make(map[Coords]struct{})
-	visited[t] = struct{}{}
-	for _, line := range lines {
-		direction := strings.Fields(line)[0]
-		steps, _ := strconv.Atoi(strings.Fields(line)[1])
-		for i := 0; i < steps; i++ {
-			h.x = h.x + transforms[direction].x
-			h.y = h.y + transforms[direction].y
-			diff := Coords{h.x - t.x, h.y - t.y}
-			if abs(diff.x) == 2 || abs(diff.y) == 2 {
-				if diff.x == 0 || diff.y == 0 {
-					t.x = t.x + transforms[direction].x
-					t.y = t.y + transforms[direction].y
-				} else {
-					if diff.x > 0 {
-						t.x = t.x + 1
-					} else {
-						t.x = t.x - 1
-					}
-					if diff.y > 0 {
-						t.y = t.y + 1
-					} else {
-						t.y = t.y - 1
-					}
-				}
-			}
-			visited[t] = struct{}{}
+			visited[knots[len(knots)-1]] = struct{}{}
 		}
 	}
 	return len(visited)
@@ -92,8 +63,9 @@ var transforms = map[string]Coords{"L": {-1, 0}, "R": {1, 0}, "U": {0, 1}, "D": 
 func main() {
 	input, _ := os.ReadFile("./input.txt")
 	lines := strings.Split(string(input), "\n")
-	prob1 := prob1(lines)
-	fmt.Println("Solution to problem 1 is", prob1)
-	// prob2 := calcVisitedPositions(lines, 10)
-	// fmt.Println("Solution to problem 2 is", prob2)
+
+	prob1 := calcVisitedPositions(lines, 2)
+	fmt.Println("Solution to problem 2 is", prob1)
+	prob2 := calcVisitedPositions(lines, 10)
+	fmt.Println("Solution to problem 2 is", prob2)
 }
