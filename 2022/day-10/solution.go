f3ae1661bad7a950b7d3509b93423e6c2007ab66
Harry Wright <harry.wright@and.digital>
2022-12-10 23:00:55 +0000

    Improvements

--- 2022/day-10/solution.go
+++ 2022/day-10/solution.go
@@ -10,7 +10,6 @@ import (
 func main() {
 	input, _ := os.ReadFile("./input.txt")
 	lines := strings.Split(string(input), "\n")
-
 	pixels := make([]string, 240)
 	check, x, processing := 20, 1, false
 	sigs := make([]int, 0)
@@ -18,22 +17,17 @@ func main() {
 		pixels[i] = "."
 	}
 	for i, cycle := 0, 1; i < len(lines); cycle++ {
-		// START OF CYCLE
 		if cycle == check {
 			check += 40
 			sigs = append(sigs, cycle*x)
 		}
-		pixelHorizontal := (cycle % 40) - 1
-		if pixelHorizontal == -1 {
-			pixelHorizontal = 39
-		}
+		pixelHorizontal := (cycle - 1) % 40
 		if pixelHorizontal == x || pixelHorizontal == x-1 || pixelHorizontal == x+1 {
 			pixels[cycle-1] = "#"
 		}
 		line := lines[i]
 		if processing {
 			processing = false
-			// END OF CYCLE
 			i += 1
 			amountToAdd, _ := strconv.Atoi(strings.Fields(line)[1])
 			x += amountToAdd
@@ -45,16 +39,15 @@ func main() {
 			}
 		}
 	}
+	sum := 0
+	for _, sig := range sigs {
+		sum += sig
+	}
+	fmt.Println("Solution to problem 1 is", sum)
 	for i, pixel := range pixels {
 		fmt.Print(pixel)
 		if (i+1)%40 == 0 {
 			fmt.Println()
 		}
 	}
-	fmt.Println()
-	sum := 0
-	for _, sig := range sigs {
-		sum += sig
-	}
-	fmt.Println("Solution to problem 1 is", sum)
 }
