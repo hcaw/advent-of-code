6eff4f2de289c79d6157736f268e3a12ca500ad1
Harry Wright <harry.wright@and.digital>
2022-12-10 20:29:00 +0000

    Solution to problem 2

--- 2022/day-10/solution.go
+++ 2022/day-10/solution.go
@@ -11,13 +11,29 @@ func main() {
 	input, _ := os.ReadFile("./input.txt")
 	lines := strings.Split(string(input), "\n")
 
+	pixels := make([]string, 240)
 	check, x, processing := 20, 1, false
 	sigs := make([]int, 0)
+	for i := 0; i < len(pixels); i++ {
+		pixels[i] = "."
+	}
 	for i, cycle := 0, 1; i < len(lines); cycle++ {
 		// START OF CYCLE
 		if cycle == check {
 			check += 40
 			sigs = append(sigs, cycle*x)
+		}
+		// The middle of the sprite is x
+		// The location of the pixel is the cycle
+		// Translate cycle position into horizontal position
+
+		pixelHorizontal := (cycle % 40) - 1 // 0 - 39
+		if pixelHorizontal == -1 {
+			pixelHorizontal = 39
+		}
+		// Check here if the location of the current pixel matches one of the locations of the sprite
+		if pixelHorizontal == x || pixelHorizontal == x-1 || pixelHorizontal == x+1 {
+			pixels[cycle-1] = "#"
 		}
 		line := lines[i]
 		if processing {
@@ -34,6 +50,13 @@ func main() {
 			}
 		}
 	}
+	for i, pixel := range pixels {
+		fmt.Print(pixel)
+		if (i+1)%40 == 0 {
+			fmt.Println()
+		}
+	}
+	fmt.Println()
 	sum := 0
 	for _, sig := range sigs {
 		sum += sig
