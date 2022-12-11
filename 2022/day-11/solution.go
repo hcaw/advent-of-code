3a4c07db7da24523c675d66592873f2e4d196d87
Harry Wright <harry.wright@and.digital>
2022-12-11 09:25:26 +0000

    Initialised monkey data

--- 2022/day-11/solution.go
+++ 2022/day-11/solution.go
@@ -10,7 +10,7 @@ import (
 type Monkey struct {
 	items   []int
 	opArith string
-	opNum   int
+	operand   string
 	testDiv int
 	testT   int
 	testF   int
@@ -20,7 +20,6 @@ func main() {
 	input, _ := os.ReadFile("./input.txt")
 	monkeyGroups := strings.Split(string(input), "\n\n")
 
-	// initialise monkey data
 	monkeys := make([]Monkey, len(monkeyGroups))
 	for i, monkey := range monkeyGroups {
 		lines := strings.Split(string(monkey), "\n")
@@ -33,6 +32,14 @@ func main() {
 			items = append(items, num)
 		}
 		monkey.items = items
+		monkey.opArith = strings.Fields(lines[2])[4]
+		monkey.operand = strings.Fields(lines[2])[5]
+		testDiv, _ := strconv.Atoi(strings.Fields(lines[3])[3])
+		monkey.testDiv = testDiv
+		testT, _ := strconv.Atoi(strings.Fields(lines[4])[5])
+		testF, _ := strconv.Atoi(strings.Fields(lines[5])[5])
+		monkey.testT = testT
+		monkey.testF = testF
 		monkeys[i] = monkey
 	}
 	fmt.Println(monkeys)
